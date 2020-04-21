cabfe6798b1a61f47707d19d36e7890d558f3148
Huy Hoang Lam <huy.lam@okiela.com>
2020-04-21 10:17:01 +0700

    ADD: init project

---
+++ SPHTechTest/SPHTechTest/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  SPHTechTest
+//
+//  Created by OkieLa Dev on 4/21/20.
+//  Copyright © 2020 none. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
---
+++ SPHTechTest/SPHTechTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SPHTechTest
+//
+//  Created by OkieLa Dev on 4/21/20.
+//  Copyright © 2020 none. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
